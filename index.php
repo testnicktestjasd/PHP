dff97442c7f8be3f4a16f2313637a64fbaf35764
folxu <32227397+folxu@users.noreply.github.com>
2017-10-10 18:42:01 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Logowanie</title>
+</head>
+<body>
+<b>Logowanie</b>
+
+<form action="zalogowano.php" method="POST">
+Nick:
+<input type="text" name="login" /> <br>
+Hasło:
+<input type="password" name="haslo" /> <br>
+<input type="submit" name="Zaloguj!" />
+
+</form>
+
+</body>
+</html>
