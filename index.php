bf970bd01642fee090721418712e2ecc3fdd84ce
Adam <32227397+folxu@users.noreply.github.com>
2017-10-27 23:10:05 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Tematyka do zrobienia</title>
+	<link rel="stylesheet" href="style/style.css" type="text/css" />
+	<link href="https://fonts.googleapis.com/css?family=Lato:400,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+</head>
+<body>
+      <div id="container">
+
+           <div id="logo">
+                Cheaty do gier.
+           </div>
+
+           <div id="topbar">
+           </div>
+           
+           <div id="logowanie">
+           to niezle logowanie...
+           </div>
+
+           <div id="sidebar" > <!-- Menu boczne -->
+           
+            <div class="optionL">Strona główna</div>
+		       	<div class="optionL">Galeria</div>
+			      <div class="optionL">Dodaj Usera</div>
+			      <div class="optionL">Dodaj wpis</div>
+            
+           </div>
+
+           <div id="contentt">
+           <!-- Wyswietlenie strony w iframe! -->
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia mollis odio eu bibendum. Praesent non hendrerit risus. Nulla id semper sem. Mauris risus mauris, ultrices sed ullamcorper sed, vulputate vel nisi. Aliquam augue ante, mattis in pulvinar vitae, ultrices nec leo. Nulla ultricies augue enim, sit amet semper tellus vulputate sit amet. Maecenas tincidunt, ex eu viverra scelerisque, quam lectus auctor nunc, at pretium nibh lacus in ligula. Cras condimentum felis ac aliquet tristique. Sed elementum eu nulla vel rutrum. Cras feugiat nulla non congue malesuada. 
+           <br /><br />
+           Cras et nulla vehicula, efficitur enim non, fermentum tortor. Curabitur id elementum leo. Sed eget turpis accumsan dolor mollis imperdiet. Praesent pellentesque laoreet lectus, at commodo magna varius vitae. Aliquam erat volutpat. Curabitur commodo, tortor laoreet sagittis cursus, nulla enim laoreet libero, et egestas risus ante vel orci. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc quis posuere massa, sed sollicitudin lorem. Mauris lacinia, massa efficitur malesuada luctus, arcu ex mattis erat, a venenatis magna risus nec neque. Nulla vulputate nisl urna, quis egestas orci suscipit tristique. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras auctor nec elit at ultricies. Morbi aliquam pharetra diam, vitae porta felis. Pellentesque vel arcu tincidunt, luctus justo quis, ultrices erat. Vivamus efficitur leo vitae dui molestie, eu varius sapien iaculis. In quis pharetra mauris. 
+           <br /><br />
+           Nam ullamcorper turpis non tristique sollicitudin. Etiam id magna lacus. Pellentesque vestibulum ex eget quam consectetur, sit amet luctus erat feugiat. Sed gravida tellus tempus consequat rhoncus. Phasellus lobortis magna et risus pharetra, facilisis blandit sapien tristique. Vivamus aliquam interdum arcu, eget facilisis ante gravida ut. Proin nec nisl ut lacus finibus sagittis id non nibh. Donec volutpat pretium libero. Sed fermentum vel ante vitae mattis. Curabitur porttitor turpis at scelerisque auctor. Sed vitae iaculis risus, ut iaculis nibh.
+           </div>
+
+           <div id="footer">
+           &copy; <a href="http://Piwnica-Speak.pl" target="Pogchamp">Piwnica-Speak.pl</a>
+           </div>
+
+      </div>
+</body>
+</html>
